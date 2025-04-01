950ba3a0e6bc26bf8160742ce14f723b6c2b20ec
Maya Schneider <myschne@umich.edu>
2025-04-01 07:46:15 -0400

    final

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    document.main.classList.toggle('dark-mode');
+    
+});
